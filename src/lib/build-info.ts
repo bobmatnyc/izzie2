e902b1aee773b216bd29fe0fc38faa13964dab75
Bob Matsuoka <bob@matsuoka.com>
2026-01-26 20:29:57 -0500

    chore: update build info

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -5,11 +5,11 @@
 
 export const BUILD_INFO = {
   version: '1.0.4',
-  gitHash: '79333c6',
+  gitHash: '9b54651',
   gitBranch: 'preview',
-  buildTime: '2026-01-26T20:24:26Z',
+  buildTime: '2026-01-27T01:30:12Z',
   nodeVersion: 'v25.2.1',
-  isDirty: false,
+  isDirty: true,
 } as const;
 
 export type BuildInfo = typeof BUILD_INFO;
